4dcea11d13222823e20471b97e095bf3d241fc35
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-11-25 09:44:48 +0000

    intlist script

---
+++ stampedlock-intlist.sh
@@ -0,0 +1,14 @@
+#
+# This class forms part of the Thread Safety with Phaser,
+# StampedLock and VarHandle Talk by Dr Heinz Kabutz from
+# JavaSpecialists.eu and may not be distributed without written
+# consent.
+#
+# (C)opyright 2020 Heinz Max Kabutz, All rights reserved.
+#
+
+git pull
+cd src
+javac -d ../out/production/modern-synchronizers eu/javaspecialists/concurrent/playground/stampedlock/IntList*.java
+cd ..
+java -cp out/production/modern-synchronizers eu/javaspecialists/concurrent/playground/stampedlock/IntListTest
